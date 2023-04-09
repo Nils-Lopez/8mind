00e55e288fb08b17f9f286aab1ebf6669b932dc0
Nils Lopez <lopez.nils@codawalp.io>
2023-04-09 22:58:57 +0200

    final ghostraise

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App({param}) {
           <div className="container btn-ctn mt-0 mb-2 pt-0">
             <div className="columns is-flex links mt-0 pt-0  pb-0 mb-0">
               <div className="column is-half mb-0 pb-0">
-                            <a className="button mt-5 btn-links">
+                            <a href="https://youtu.be/na72mqSpklw" className="button mt-5 btn-links">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="icon mr-2">
 <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>
               <strong>YOUTUBE</strong>
